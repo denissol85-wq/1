fdf013f490fd453c3b6191b304e8cc752ed16f2c
Denis <denissol85@gmail.com>
2025-11-03 19:11:41 +0200

    Update Django settings for production

--- letter-project/letter/settings.py
+++ letter-project/letter/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lyu9vs#!ra^+7tr6hv+k&@f41w7n&6vt8n2hg5*(_z^#1640eg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.render.com']
 
 
 # Application definition
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
